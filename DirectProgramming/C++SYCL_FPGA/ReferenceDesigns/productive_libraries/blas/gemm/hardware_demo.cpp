1aab69fbc0d5b6c3492ee2339cf26c9eb7eeb042
someoneinjd <someoneinjd@outlook.com>
2023-04-12 17:47:55 +0800

    Fix GEMM (Hardware)

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/hardware_demo.cpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/hardware_demo.cpp
@@ -2,33 +2,56 @@
 #include <iostream>
 #include <vector>
 
-#include "interface.hpp"
+#include "sgemm.sycl.h"
+#include "parameters.h"
+#include "HalideBuffer.h"
 
 int main() {
-    constexpr int M = 8192;
-    constexpr int N = 8192;
-    constexpr int K = 8192;
+    using Halide::Runtime::Buffer;
+    constexpr int M = III * II * 32;
+    constexpr int N = JJJ * JJ * 32;
+    constexpr int K = KKK * KK * 32;
 
-    constexpr int lda = 8192, ldb = 8192, ldc = 8192;
     constexpr auto alpha = 2.0f;
     constexpr auto beta = 3.0f; 
 
-    std::vector<float> a(M * lda);
-    std::vector<float> b(K * ldb);
-    std::vector<float> c(M * ldc);
+    Buffer<float> a(K, M);
+    Buffer<float> b(N, K);
+    Buffer<float> c(N, M);
+    Buffer<float> out(JJJ, JJ, II, III, 32, 32);
     for (size_t i = 0; i < M; i++)
         for (size_t k = 0; k < K; k++)
-            a[k + i * lda] = float(std::rand()) / RAND_MAX - 0.5f; 
+            a(k, i) = float(std::rand()) / RAND_MAX - 0.5f; 
 
     for (size_t k = 0; k < K; k++)
         for (size_t j = 0; j < N; j++)
-            b[j + k * ldb] = float(std::rand()) / RAND_MAX - 0.5f; 
+            b(j, k) = float(std::rand()) / RAND_MAX - 0.5f; 
 
     for (size_t i = 0; i < M; i++)
         for (size_t j = 0; j < N; j++)
-            c[j + i * ldc] = float(std::rand()) / RAND_MAX - 0.5f;
+            c(j, i) = float(std::rand()) / RAND_MAX - 0.5f;
 
-    auto exec_time = t2sp::gemm(false, false, M, N, K, alpha, a.data(), lda, b.data(), ldb, beta, c.data(), ldc);
+#if defined(SYCL_LANGUAGE_VERSION) && SYCL_LANGUAGE_VERSION >= 202001
+#if defined(FPGA_EMULATOR) || defined(TEST)
+    auto device_selector = +[](const sycl::device &device){
+        return device.get_platform().get_info<sycl::info::platform::name>()
+            == sycl::ext::intel::EMULATION_PLATFORM_NAME ? 10000 : -1;
+    };
+#else
+    auto device_selector = +[](const sycl::device &device){
+        return device.get_platform().get_info<sycl::info::platform::name>()
+            == sycl::ext::intel::HARDWARE_PLATFORM_NAME ? 10000 : -1;
+    };
+#endif
+#else
+#if defined(FPGA_EMULATOR) || defined(TEST)
+    sycl::ext::intel::fpga_emulator_selector device_selector{};
+#else
+    sycl::ext::intel::fpga_selector device_selector{};
+#endif
+#endif
+
+    auto exec_time = t2sp::sgemm::sgemm(device_selector, false, false, alpha, beta, a, b, c, out); 
 
     double number_ops = 2.0 * M * N * K + M * N;
     std::cout << "GFlops: " << number_ops / exec_time << "\n";
