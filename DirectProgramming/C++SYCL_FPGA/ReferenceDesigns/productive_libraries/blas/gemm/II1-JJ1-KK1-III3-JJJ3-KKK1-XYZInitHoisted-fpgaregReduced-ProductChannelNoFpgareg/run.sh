1b4dc233bab24986492a672296724e2b948485f4
Hongbo Rong <ronghongbo@hrong1-mobl1.amr.corp.intel.com>
2023-05-03 16:09:08 -0700

    Use fpga_tools::UnrolledLoop and NTuple

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/II1-JJ1-KK1-III3-JJJ3-KKK1-XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg/run.sh
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/II1-JJ1-KK1-III3-JJJ3-KKK1-XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg/run.sh
@@ -2,6 +2,6 @@
 set -x
 #cd /home/u89062/gemm-synthesis-issue/II8-JJ8-KK8-III8-JJJ8-KKK8
 icpx  -DA10 -DT2SP_SGEMM -DTINY -I. -fsycl -fintelfpga -o hardware_demo.o -c hardware_demo.cpp
-icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=Arria10 -fsycl-device-code-split=per_kernel -fsycl-link=early -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile  hardware_demo.o  -o gemm_fpga --verbose
-#icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=Arria10 -fsycl-device-code-split=per_kernel -fsycl-link=early -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga --verbose
+#icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=Arria10 -fsycl-device-code-split=per_kernel -fsycl-link=early -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile  hardware_demo.o  -o gemm_fpga --verbose
+icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=Arria10 -fsycl-device-code-split=per_kernel -fsycl-link=early -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga --verbose
 
