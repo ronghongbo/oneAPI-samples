bb631b1614cde4ceeb82b311ad4e68fa3f593106
ronghongbo <hongbo.rong@intel.com>
2023-05-04 17:35:46 -0700

    Minor fix

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/3x3/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped/synthesize.sh
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/3x3/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped/synthesize.sh
@@ -3,11 +3,10 @@
 # This does not work:
 # Path to this script
 #PATH_TO_SCRIPT="$( cd "$(dirname "$BASH_SOURCE" )" >/dev/null 2>&1 ; pwd -P )"
-#echo Entering $PATH_TO_SCRIPT ....
 #cd $PATH_TO_SCRIPT
-cd $HOME/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/10x8/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped
+cd $HOME/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/3x3/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped
 
-icpx  -DA10 -DT2SP_SGEMM -I./include -I../../../../../include/  -fsycl -fintelfpga -o hardware_demo.o -c hardware_demo.cpp
+icpx  -DA10 -DT2SP_SGEMM -DTINY -I./include -I../../../../../include/  -fsycl -fintelfpga -o hardware_demo.o -c hardware_demo.cpp
 #icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=intel_a10gx_pac:pac_a10 -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga --verbose
 icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=Arria10 -fsycl-device-code-split=per_kernel -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga --verbose
 
