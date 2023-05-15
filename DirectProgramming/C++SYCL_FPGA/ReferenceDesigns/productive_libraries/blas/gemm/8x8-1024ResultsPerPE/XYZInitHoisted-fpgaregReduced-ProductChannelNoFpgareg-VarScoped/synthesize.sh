726d1bcc7681afc952467c49280c389883f0569b
ronghongbo <hongbo.rong@intel.com>
2023-05-15 16:58:25 -0700

    Update libHalide, which minimizes the scope of shift registers.

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/8x8-1024ResultsPerPE/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped/synthesize.sh
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/8x8-1024ResultsPerPE/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped/synthesize.sh
@@ -1,4 +1,5 @@
 #devcloud_login -b A10OAPI walltime=24:00:00 ./synthesize.sh
+#source /glob/development-tools/versions/oneapi/2023.1.4/oneapi/setvars.sh
 cd /home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/8x8-1024ResultsPerPE/XYZInitHoisted-fpgaregReduced-ProductChannelNoFpgareg-VarScoped
 icpx  -DA10 -DT2SP_SGEMM -I./include -fsycl -fintelfpga -o hardware_demo.o -c hardware_demo.cpp
 icpx -rdynamic -fsycl -fintelfpga -Xshardware -Xstarget=intel_a10gx_pac:pac_a10 -Xsffp-reassociate -Xsffp-contract=fast -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga --verbose
