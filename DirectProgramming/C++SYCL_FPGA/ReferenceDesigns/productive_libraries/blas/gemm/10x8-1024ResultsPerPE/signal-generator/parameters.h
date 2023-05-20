f8a1e375b95ab17bcc3aa4d79958947ec15d5916
ronghongbo <hongbo.rong@intel.com>
2023-05-20 08:18:11 -0700

    Add 10x8-1024ResultsPerPE/signal-generator. Synthesis success! 224 MHZ.

---
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/10x8-1024ResultsPerPE/signal-generator/parameters.h
@@ -0,0 +1,123 @@
+/*******************************************************************************
+* Copyright 2021 Intel Corporation
+*
+* Licensed under the BSD-2-Clause Plus Patent License (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://opensource.org/licenses/BSDplusPatent
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+*
+* SPDX-License-Identifier: BSD-2-Clause-Patent
+*******************************************************************************/
+#ifndef GEMM_CONST_PARAMS_H
+#define GEMM_CONST_PARAMS_H
+
+// Inner loop bounds, which are static constant parameters of the design
+#ifdef TINY // For verifying correctness only
+            #define KKK         16
+            #define JJJ         8
+            #define III         10
+            #define JJ          32
+            #define II          32
+            #define KK          32
+#else // LARGE
+    #ifdef GPU
+        #define KKK         8
+        #define JJJ         8
+        #define III         32
+        #define JJ          8
+        #define II          2
+        #define KK          1
+    #elif defined(S10)
+        #ifdef T2SP_SGEMM
+            #define KKK         16
+            #define JJJ         16
+            #define III         14
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #elif defined(T2SP_DGEMM)
+            #define KKK         8
+            #define JJJ         4
+            #define III         8
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #elif defined(T2SP_CGEMM)
+            #define KKK         16
+            #define JJJ         16
+            #define III         14
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #else
+            #define KKK         4
+            #define JJJ         6
+            #define III         4
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #endif
+    #else   // TARGET == A10
+        #ifdef T2SP_SGEMM
+            #define KKK         16
+            #define JJJ         8
+            #define III         10
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #elif defined(T2SP_DGEMM)
+            #define KKK         8
+            #define JJJ         4
+            #define III         8
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #elif defined(T2SP_CGEMM)
+            #define KKK         8
+            #define JJJ         4
+            #define III         10
+            #define JJ          32
+            #define II          32
+            #define KK          32
+        #else
+            #define KKK         4
+            #define JJJ         4
+            #define III         4
+            #define JJ          32
+            #define II          32
+            #define KK          32        
+        #endif
+    #endif
+#endif
+
+#if defined(T2SP_SGEMM)
+    #define ZERO 0
+    #define CONST_TYPE float
+    #define TTYPE Float(32)
+    #define KERNEL "sgemm"
+#elif defined(T2SP_DGEMM)
+    #define ZERO 0
+    #define CONST_TYPE double
+    #define TTYPE Float(64) 
+    #define KERNEL "dgemm"
+#elif defined(T2SP_CGEMM)
+    #define ZERO complex32_t(0.0f, 0.0f)
+    #define CONST_TYPE complex32_t
+    #define TTYPE Complex(32)
+    #define KERNEL "cgemm"
+#elif defined(T2SP_ZGEMM)
+    #define ZERO complex64_t(0.0, 0.0)
+    #define CONST_TYPE complex64_t
+    #define TTYPE Complex(64) 
+    #define KERNEL "zgemm"
+#endif
+
+#endif 
---
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/10x8-1024ResultsPerPE/signal-generator/synthesize.sh
@@ -0,0 +1,28 @@
+#devcloud_login -b A10OAPI walltime=24:00:00 ./synthesize.sh
+source /glob/development-tools/versions/oneapi/2023.1.2/oneapi/setvars.sh
+cd /home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/10x8-1024ResultsPerPE/signal-generator
+set -x
+SYCL_LINK=image #early
+
+#icpx  -DA10 -DT2SP_SGEMM -DTINY -I/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../tools/Halide/include -I.  -I/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../../../include  -std=gnu++11 -o gemm.o -c gemm.cpp
+
+#icpx  -rdynamic gemm.o  -o gemm_generate_tiny_sgemm  -L/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../tools/Halide/lib -Wl,-rpath,/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../tools/Halide/lib -lpthread -lz -ldl -lHalide 
+
+#env CLEARCODE=1 ./gemm_generate_tiny_sgemm
+
+icpx  -DA10 -DT2SP_SGEMM -DTINY -I/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../tools/Halide/include -I.  -I/home/u89062/oneAPI-samples-ronghongbo/DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/../../../../include  -fsycl -fintelfpga -o  hardware_demo.o -c hardware_demo.cpp
+
+icpx    -rdynamic -fsycl -fintelfpga -reuse-exe=gemm_fpga -Xshardware -Xstarget=/opt/a10/inteldevstack/a10_gx_pac_ias_1_2_1_pv/opencl/opencl_bsp:pac_a10 -Xsffp-reassociate -Xsffp-contract=fast -Xsdsp-mode=prefer-dsp -Xsprofile -Xsclock=360MHz hardware_demo.o  -o gemm_fpga
+
+
+#icpx -DA10 -DT2SP_SGEMM -DTINY -I../../../../tools/Halide/include -I../../include  -std=gnu++11 -o gemm.o -c ../../gemm.cpp
+#icpx -rdynamic gemm.o  -o gemm_generate_tiny_sgemm  -L../../../../tools/Halide/lib -Wl,-rpath,../../../../tools/Halide/lib -lpthread -lz -ldl -lHalide
+#env CLEARCODE=1 ./gemm_generate_tiny_sgemm 
+
+# Test
+#icpx -DFPGA_EMULATOR -DT2SP_NDEBUG -DMKL_ILP64 -DT2SP_TEST_0 -O0 -g -DTINY -I../../../../tools/Halide/include -I../../../../../../include -I../../include/ -I. -I/glob/development-tools/versions/oneapi/2023.1.2/oneapi/mkl/2023.1.0/include -fsycl -fintelfpga -o test.o -c ./test.cpp
+#icpx -rdynamic -fsycl -fintelfpga test.o -O0 -g  -o test_0 -L../../../../tools/Halide/lib  -L/glob/development-tools/versions/oneapi/2023.1.2/oneapi/mkl/2023.1.0/lib/intel64 -Wl,-rpath,../../../../tools/Halide/lib:/glob/development-tools/versions/oneapi/2023.1.2/oneapi/mkl/2023.1.0/lib/intel64 -lmkl_sycl -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lsycl -lOpenCL -lpthread -lm -ldl 
+
+#icpx -DA10 -DT2SP_SGEMM -DTINY -I../../../../tools/Halide/include -I../../../../../../include -I../../include/ -I. -fsycl -fintelfpga -o hardware_demo.o -c ./hardware_demo.cpp
+#icpx -rdynamic -fsycl -fintelfpga -fsycl-link=$SYCL_LINK -Xshardware -Xstarget=/opt/a10/inteldevstack/a10_gx_pac_ias_1_2_1_pv/opencl/opencl_bsp:pac_a10 -Xsffp-reassociate -Xsffp-contract=fast -Xsdsp-mode=prefer-dsp -Xsprofile -Xsclock=360MHz hardware_demo.o  -o hardware_demo
+
