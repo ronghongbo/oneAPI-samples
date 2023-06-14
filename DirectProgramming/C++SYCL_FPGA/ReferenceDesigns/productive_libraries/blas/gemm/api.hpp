ca888a1521feb10ddeaeee3f93c8328f99a2b9b7
Hongbo Rong <ronghongbo@hrong1-mobl1.amr.corp.intel.com>
2023-06-14 16:28:18 -0700

    GEMM: Merge api.cpp into api.hpp. Finish draft code of demo.cpp calling that api. To test

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/api.hpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/api.hpp
@@ -4,6 +4,16 @@
 #include <sycl/sycl.hpp>
 #include "oneapi/mkl.hpp"
 
+// API of the reconfigurable matrix multiplication.The interface will be invoked by the GEMM implementation below.
+#include "reconfigurable_matmul/api.hpp"
+
+// Parameters of the reconfigurable matrix multiplication.
+#include "reconfigurable_matmul/parameters.hpp"
+
+// Data structures, etc. in Halide/T2SP
+#include "Halide.h"
+using namespace Halide;
+
 namespace t2sp::blas::row_major {
 // The API for GEMM. We choose the USM version of oneMKL DPC++ interface (https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-dpcpp/2023-0/gemm.html) with the
 // restriction of standard data types (s, d, c, z) only. In this case, the matrices, alpha and beta all have the same data type according to the DPC++ interface. So we define our
@@ -24,5 +34,74 @@ sycl::event gemm(sycl::queue &queue,
                  T *c,
                  std::int64_t ldc,
                  compute_mode mode = compute_mode::unset,
-                 const std::vector<sycl::event> &dependencies = {});
+                 const std::vector<sycl::event> &dependencies = {}) {
+    bool transpose_a = (transa == oneapi::mkl::transpose::T || transa == oneapi::mkl::transpose::C);
+    bool transpose_b = (transb == oneapi::mkl::transpose::T || transb == oneapi::mkl::transpose::C);
+    bool conjugate_a = (transa == oneapi::mkl::transpose::C);
+    bool conjugate_b = (transb == oneapi::mkl::transpose::C);
+
+    // Check parameters for constraints set by oneMKL DPC++ interface (https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-dpcpp/2023-0/gemm.html)
+    _halide_user_assert(m >= 0 && n >= 0 && k >= 0) << "m = " << m << ", n = " << n << ", k = " << k;
+    _halide_user_assert(a && b && c) << "a = " << (const void *)a << ", b = " << (const void *)b << ", c = " << (const void *)c;
+    _halide_user_assert(!transpose_a && lda >= k || transpose_a && lda >= m)
+        << std::boolalpha << "transa = " << transa << ", lda = " << lda << ", m = " << m << ", k = " << k;
+    _halide_user_assert(!transpose_b && ldb >= n || transpose_b && ldb >= k)
+        << std::boolalpha << "transb = " << transb << ", ldb = " << ldb << ", n = " << n << ", k = " << k;
+    _halide_user_assert(ldc >= n) << "ldc = " << ldc << ", n = " << n;
+
+    _halide_user_assert(constexpr (std::is_same_v<float, T>) || constexpr (std::is_same_v<double, T>) || constexpr (std::is_same_v<std::complex<float>, T>) ||
+                        constexpr (std::is_same_v<std::complex<double>, T>)) << "Unsupported data type";
+
+    // Constraints due to this implementation
+    _halide_user_assert(mode == compute_mode::unset) << "The current implementation supports only compute_mode::unset";
+    // TOREMOVE: These two constraints below should be checked by the reconfigurable matmul instead.
+    _halide_user_assert(n % JJJ == 0) << "For performance reasons, the current implementation requires that n must be a multiple of " << JJJ
+                              << "(the vectorized dimension for the output matrix), but n = " << n;
+    _halide_user_assert(k % KKK == 0) << "For performance reasons, the current implementation requires that k must be a multiple of " << KKK
+                              << "(the vectorized dimension for the input matrices), but k = " << k;
+
+    using Halide::Runtime::Buffer;
+    std::int64_t rows_of_A = !transpose_a ? m : k;
+    std::int64_t cols_of_A = !transpose_a ? k : m;
+    std::int64_t rows_of_B = !transpose_b ? k : n;
+    std::int64_t cols_of_B = !transpose_b ? n : k;
+    halide_dimension_t dim_a[]{{0, cols_of_A, 1}, {0, rows_of_A, lda}};
+    Buffer<T> A_buffer{const_cast<T *>(a), 2, dim_a};
+    halide_dimension_t dim_b[]{{0, cols_of_B, 1}, {0, rows_of_B, ldb}};
+    Buffer<T> B_buffer{const_cast<T *>(b), 2, dim_b};
+    halide_dimension_t dim_c[]{{0, n, 1}, {0, m, ldc}};
+    Buffer<T> C_buffer{c, 2, dim_c};
+    Buffer<T> Output_buffer{JJJ, JJ, II, III, (n + (JJJ * JJ - 1)) / (JJJ * JJ), (m + (III * II - 1)) / (III * II)};
+
+    for (sycl::event e : dependencies) {
+        e.wait();
+    }
+
+    bool FromSymmetricPosA = transpose_a;
+    bool FromSymmetricPosB = transpose_b;
+    bool FromSymmetricPosC = false;
+    bool ConjugateA = conjugate_a;
+    bool ConjugateB = conjugate_b;
+    bool ConjugateC = false;
+    bool HalfSpace = false;
+
+    if constexpr (std::is_same_v<float, T>) {
+        return t2sp::blas::row_major::smatmul(queue, FromSymmetricPosA, FromSymmetricPosB, FromSymmetricPosC,
+                                              ConjugateA, ConjugateB, ConjugateC, HalfSpace, alpha, beta,
+                                              A_buffer, B_buffer, C_buffer, Output_buffer);
+    } else if constexpr (std::is_same_v<double, T>) {
+        return t2sp::blas::row_major::dmatmul(queue, FromSymmetricPosA, FromSymmetricPosB, FromSymmetricPosC,
+                                              ConjugateA, ConjugateB, ConjugateC, HalfSpace, alpha, beta,
+                                              A_buffer, B_buffer, C_buffer, Output_buffer);
+    } else if constexpr (std::is_same_v<std::complex<float>, T>) {
+        return t2sp::blas::row_major::cmatmul(queue, FromSymmetricPosA, FromSymmetricPosB, FromSymmetricPosC,
+                                              ConjugateA, ConjugateB, ConjugateC, HalfSpace, alpha, beta,
+                                              A_buffer, B_buffer, C_buffer, Output_buffer);
+    } else {
+        return t2sp::blas::row_major::zmatmul(queue, FromSymmetricPosA, FromSymmetricPosB, FromSymmetricPosC,
+                                              ConjugateA, ConjugateB, ConjugateC, HalfSpace, alpha, beta,
+                                              A_buffer, B_buffer, C_buffer, Output_buffer);
+    }
 }
+
+} // namespace t2sp::blas::row_major
--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/demo.cpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/productive_libraries/blas/gemm/demo.cpp
@@ -1,58 +1,94 @@
-// To compile this file, pass in a macro for the compute (T2SP_S/D/C/ZMATMUL), the size of the systolic array (TINY or LARGE), and the hardware(A10 or S10).
+// To compile this file, pass in a macro for the compute (T2SP_S/D/c/ZMATMUL), the size of the systolic array (TINY or LARGE), and the hardware(A10 or S10).
 // And pass in a macro FPGA_EMULATOR if to use the emulator instead of FPGA hardware.
 
 #include <cstdlib>
 #include <iostream>
 
-// The programming interface of the reconfigurable matrix multiplication.
-#include "reconfigurable_matmul/api.hpp"
+// The GEMM API to invoke
+#include "./api.hpp"
 
-CONST_TYPE get_random() {
-#if defined(T2SP_SMATMUL) || defined(T2SP_DMATMUL)
-    return (CONST_TYPE) (std::rand() / RAND_MAX - 0.5f);
+// Useful routines from the OneMKL unit tests
+#include "unit_tests/blas/include/test_common.hpp"
+
+using namespace std;
+
+template <typename T>
+void test(oneapi::mkl::transpose transa, oneapi::mkl::transpose transb,
+          int64_t m, int64_t n, int64_t k, T alpha, int64_t lda, int64_t ldb, T beta, int64_t ldc) {
+    vector<T, allocator_helper<T, 64>> a, b;
+    vector<T, allocator_helper<T, 64>> c, c_ref;
+    rand_matrix(a, oneapi::mkl::layout::row_major, transa, m, k, lda);
+    rand_matrix(b, oneapi::mkl::layout::row_major, transb, k, n, ldb);
+    rand_matrix(c, oneapi::mkl::layout::row_major, oneapi::mkl::transpose::nontrans, m, n, ldc);
+    c_ref = c;
+
+// Select either the FPGA emulator or FPGA device
+#if defined(FPGA_EMULATOR)
+    ext::intel::fpga_emulator_selector device_selector;
 #else
-    return (CONST_TYPE) { std::rand() / RAND_MAX - 0.5f, std::rand() / RAND_MAX - 0.5f };
+    ext::intel::fpga_selector          device_selector;
 #endif
+
+     // Create a queue bound to the chosen device.
+     // If the device is unavailable, a SYCL runtime exception is thrown.
+     sycl::queue q_device(device_selector, fpga_tools::exception_handler);
+
+     sycl::event e = t2sp::blas::row_major::gemm<float>(q_device, transa, transb, m, n, k, alpha, a.data(), lda,
+                                                        b.data(), ldb, beta, c.data(), ldc);
+
+#ifdef CHECK_CORRECTNESS
+    // Call oneMKL GEMM as reference.
+    sycl::queue main_queue(sycl::cpu_selector_v);
+    oneapi::mkl::blas::row_major::gemm(main_queue, transa, transb, m, n, k, alpha, a.data(), lda,
+                                       b.data(), ldb, beta, c_ref.data(), ldc);
+    bool correct = check_equal_matrix(c.data(), c_ref.data(), oneapi::mkl::layout layout::row_major, m, n, ldc,  10 * k, std::cout);
+    assert(correct);
+#endif
+
+    // Get time in ns
+    uint64_t start = e.get_profiling_info<sycl::info::event_profiling::command_start>();
+    uint64_t end   = e.get_profiling_info<sycl::info::event_profiling::command_end>();
+    uint64_t exec_time = end - start;
+    std::cout << "Execution time in nanoseconds = " << exec_time << "\n";
+
+    if (constexpr (std::is_same_v<float, T>) || constexpr (std::is_same_v<double, T>)) {
+        // FP operations per MAD (MUL and ADD) for float and double =2
+        double number_ops = 2.0 * m * n * k + m * n;
+    } else {
+        // FP operations per MAD (MUL and ADD) for complex float and double =8:
+        // Multiplying two complex numbers requires 4 FP MUL and 2 FP ADD
+        // Adding two complex numbers requires 2 FP ADD
+        double number_ops = 8.0 * m * n * k + 2.0 * m * n;
+    }
+    std::cout << "GFLOPs: " << number_ops / exec_time << "\n";
+    std::cout << "Size of matrix a: " << m << " * " << k << "\n";
+    std::cout << "Size of matrix b: " << k << " * " << n << "\n";
+    std::cout << "Size of matrix c: " << m << " * " << n << "\n";
 }
 
 int main() {
-    constexpr int64_t M = III * II * 32;
-    constexpr int64_t N = JJJ * JJ * 32;
-    constexpr int64_t K = KKK * KK * 32;
-    constexpr int64_t lda = K;
-    constexpr int64_t ldb = N;
-    constexpr int64_t ldc = N;
+    constexpr int64_t m = III * II * 32;
+    constexpr int64_t n = JJJ * JJ * 32;
+    constexpr int64_t k = KKK * KK * 32;
+    constexpr int64_t lda = k;
+    constexpr int64_t ldb = n;
+    constexpr int64_t ldc = n;
 
-    constexpr CONST_TYPE alpha = 2.0f;
-    constexpr CONST_TYPE beta = 3.0f;
-
-    CONST_TYPE a[M][K], b[K][N], c[M][N];
-    for (size_t i = 0; i < M; i++)
-        for (size_t k = 0; k < K; k++)
-            a[i][k] = get_random();
-
-    for (size_t k = 0; k < K; k++)
-        for (size_t j = 0; j < N; j++)
-            b[k][j] = get_random();
-
-    for (size_t i = 0; i < M; i++)
-        for (size_t j = 0; j < N; j++)
-            c[i][j] = get_random();
-
-    auto exec_time = t2sp::blas_matmul(false, false, M, N, K, alpha, a, lda, b, ldb, beta, c, ldc);
-
-#if defined(T2SP_SMATMUL) || defined(T2SP_DMATMUL)
-    // FP operations per MAD (MUL and ADD) for float and double =2
-    double number_ops = 2.0 * M * N * K + M * N;
+#if defined(T2SP_SMATMUL)
+    constexpr float alpha = 2.0f;
+    constexpr float beta  = 3.0f;
+    test<float>(oneapi::mkl::transpose::N, oneapi::mkl::transpose::T, m, n, k, alpha, lda, ldb, beta, ldc);
+#elif defined(T2SP_DMATMUL)
+    constexpr double alpha = 2.0f;
+    constexpr double beta = 3.0f;
+    test<double>(oneapi::mkl::transpose::N, oneapi::mkl::transpose::T, m, n, k, alpha, lda, ldb, beta, ldc);
+#elif defined(T2SP_CMATMUL)
+    constexpr std::complex<float> alpha = {2.0f, -0.5f};
+    constexpr std::complex<float> beta  = {3.0f, -1.5f};
+    test<std::complex<float>>(oneapi::mkl::transpose::N, oneapi::mkl::transpose::T, m, n, k, alpha, lda, ldb, beta, ldc);
 #else
-    // FP operations per MAD (MUL and ADD) for complex float and double =8:
-    // Multiplying two complex numbers requires 4 FP MUL and 2 FP ADD
-    // Adding two complex numbers requires 2 FP ADD
-    double number_ops = 8.0 * M * N * K + 2.0 * M * N;
+    constexpr std::complex<double> alpha = {2.0f, -0.5f};
+    constexpr std::complex<double> beta  = {3.0f, -1.5f};
+    test<std::complex<double>>(oneapi::mkl::transpose::N, oneapi::mkl::transpose::T, m, n, k, alpha, lda, ldb, beta, ldc);
 #endif
-    std::cout << "GFLOPs: " << number_ops / exec_time << "\n";
-    std::cout << "Size of matrix A: " << M << " * " << K << "\n";
-    std::cout << "Size of matrix B: " << K << " * " << N << "\n";
-    std::cout << "Size of matrix C: " << M << " * " << N << "\n";
-    return 0;
 }
